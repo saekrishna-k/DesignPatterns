281ddfda67c0d473a6bd1e6cd40729ccec2cfd1d
krish-na-7 <kvskrishna712@gmail.com>
2023-06-30 14:46:31 +0530

    Added Factory Design Pattern

---
+++ src/creational/factory/ShapeFactory.java
@@ -0,0 +1,14 @@
+package creational.factory;
+
+public class ShapeFactory {
+    Shape getShape(String input) {
+        switch (input) {
+            case "RECTANGLE":
+                return new Rectangle();
+            case "CIRCLE":
+                return new Circle();
+            default:
+                return null;
+        }
+    }
+}
